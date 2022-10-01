a492d3b2a34f2b4d50b4d32414727257ad80f4e8
Reky Bongso <rekybongso@gmail.com>
2022-10-01 18:42:11 +0800

    Initial commit

---
+++ app/src/main/java/me/rekyb/storyapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package me.rekyb.storyapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.rekyb.storyapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
